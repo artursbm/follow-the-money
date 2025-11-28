bcd476e2d797161ff3eef72a1f97c495566981dd
Artur Mello <artursbmello@gmail.com>
2025-11-28 19:02:33 -0300

    starting user service implementation with create-new-user fn

--- src/follow_the_money/application/service/user.clj
+++ src/follow_the_money/application/service/user.clj
@@ -1,1 +1,21 @@
-(ns follow-the-money.application.service.user)
+(ns follow-the-money.application.service.user
+  (:require
+   [clojure.spec.alpha :as s]
+   [follow-the-money.domain.models.user :as user-specs]
+   [follow-the-money.infrastructure.database.in-memory-db :as db]))
+
+(defn create-new-user
+  "Handler Responsible for orchestrating the call from API.
+   Adapts the request DTO to business model,
+   validates user data model, adds ID and created-at to user,
+   saves it to database and adapts to response output."
+  [user-model]
+  (-> user-model
+      (assoc ::user-specs/id (random-uuid)
+             ::user-specs/created-at (java.time.Instant/now))
+      db/insert-user))
+
+(s/fdef create-new-user
+  :args (s/cat :user ::user-specs/model)
+  :ret ::user-specs/model)
+
