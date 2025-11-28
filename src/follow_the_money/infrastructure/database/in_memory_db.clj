3622d64855d9b73d1394e612d2f89a57cf54cbba
Artur Mello <artursbmello@gmail.com>
2025-11-28 19:02:50 -0300

    updating in-mem db insert operations

--- src/follow_the_money/infrastructure/database/in_memory_db.clj
+++ src/follow_the_money/infrastructure/database/in_memory_db.clj
@@ -1,6 +1,7 @@
 (ns follow-the-money.infrastructure.database.in-memory-db
   (:require
    [follow-the-money.application.adapters.group :as adapters.group]
+   [follow-the-money.application.adapters.user :as adapters.user]
    [follow-the-money.domain.models.group :as group-specs]))
 
 (def database (atom {:group {}
@@ -11,27 +12,19 @@
 (defn insert-group
   [group-model]
   (let [id-key     (keyword (str (::group-specs/id group-model)))
-        group-data (adapters.group/model->group-data group-model)]
+        group-data (adapters.group/model->data group-model)]
     (-> database
         (swap! update-in [:group] assoc id-key group-data)
         :group
         (get id-key)
-        (adapters.group/group-data->model))))
+        (adapters.group/data->model))))
 
 (defn insert-user
-  [user-data]
-  (->> user-data
-       (swap! database update-in [:user] conj)
-       :user))
-
-(defn insert-expense
-  [expense-data]
-  (->> expense-data
-       (swap! database update-in [:expense] conj)
-       :expense))
-
-(defn insert-payment
-  [payment-data]
-  (->> payment-data
-       (swap! database update-in [:payment] conj)
-       :payment))
+  [user-model]
+  (let [id-key     (keyword (str (::group-specs/id user-model)))
+        user-data (adapters.user/model->data user-model)]
+    (-> database
+        (swap! update-in [:user] assoc id-key user-data)
+        :user
+        (get id-key)
+        (adapters.user/data->model))))
